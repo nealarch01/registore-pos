0f225d6a6e3bb27220a295b1607e1d72cf2e8910
Jesse Hayes <jahjahjesse@gmail.com>
2022-12-02 23:04:00 -0800

    Accounting page: added WIP code for custom date range (#2)
    * Added WIP accounting page code
    
    * used a better for loop to generate data table
    
    * added test data to updatedAccounting
    
    to use until Neal shows me how to pull data from backend
    
    * added WIP custom date functionality
    
    will allow users to  to pull transaction history from a custom date range
    
    * Custom Date Range picker mostly complete
    
    just need to code data retrieval from backend
    
    * added some comments and cleaned up some formatting

--- render/js/updatedAccounting.js
+++ render/js/updatedAccounting.js
@@ -1,4 +1,6 @@
-includeHTML();
+
+
+
 //TEST DATA 
 var myList = [
     { "id": "1", "date": "11/28/2022", "salesperson_id": 5, "total": "50.55", "discount": "", "final_total": "50.55", "payment_type": "cash"},
@@ -7,44 +9,239 @@ var myList = [
   ];
 //END TEST DATA 
 
+
+
+//Old display function. timelength unused, just generates rows for all objects in myList
 function displayTable(timelength) {
-    //load transactions from dates going back timelength
-    var rows = " <caption>Data from last " + timelength + " days</caption>";
-    addColumnHeaders(myList);
-
-
-    //will need to fix for loop to adhere to # of days requested
-    for (var i = 0; i < myList.length; i++) {
-        rows += "<tr>";
-
-
-        //UPDATE THIS WHEN WE KNOW FINAL KEYS VVVVVV
-        //rows += "<td>" + myList[i].id + "</td>" + "<td>" + myList[i].date + "</td>" + "<td>" + myList[i].salesperson_id + "</td>"
-        //+ "<td>" + myList[i].total + "</td>" + "<td>" + myList[i].discount + "</td>" + "<td>" + myList[i].final_total + "</td>" + "<td>" + myList[i].payment_type + "</td>";
-        
-
-        //this should be better, wont need updating if data keys change
-        for (var key in myList[i]){
-            rows += "<td>" + myList[i][key] + "</td>";
-        }
-
-
-        rows += "</tr>";
+  //load transactions from dates going back timelength
+  var rows = " <caption>Data from last " + timelength + " days</caption>";
+  addColumnHeaders(myList);
+  for (var i = 0; i < myList.length; i++) {
+    rows += "<tr>";
+     //{ "id": "1", "date": "11/28/2022", "salesperson_id": 5, "total": "50.55", "discount": "", "final_total": "50.55", "payment_type": "cash"},
+      
+    //rows += "<td>" + myList[i].id + "</td>" + "<td>" + myList[i].date + "</td>" + "<td>" + myList[i].salesperson_id + "</td>"
+    //  + "<td>" + myList[i].total + "</td>" + "<td>" + myList[i].discount + "</td>" + "<td>" + myList[i].final_total + "</td>" + "<td>" + myList[i].payment_type + "</td>";
+      
+    for (var key in myList[i]){
+        rows += "<td>" + myList[i][key] + "</td>";
     }
-    var table = document.getElementById("datatable");
-    table.innerHTML += rows;
-    console.log(rows);
+
+      rows += "</tr>";
+  }
+  var table = document.getElementById("datatable");
+  table.innerHTML += rows;
+  console.log(rows);
 }
 
+//generates column headers based off the keys from the first object in the list
 function addColumnHeaders(myList){
-    
-    var rows = "<tr>";
-    var rowHash = myList[0];
-    for (var key in rowHash){
-        rows +=  "<th>" + key + "</th>";
-    }
-    rows += "</tr>";
+  var rows = "<tr>";
+      var rowHash = myList[0];
+      for (var key in rowHash){
+          rows +=  "<th>" + key + "</th>";
+      }
+  rows += "</tr>";
 
+  var table = document.getElementById("datatable");
+  table.innerHTML = rows;
+}
+
+
+//generates a table containing all transactions from first of year until today.
+function YTDTable(){
+  var today = new Date(); //= get todays date
+  var firstDayOfYear = Date(today.getFullYear, 1, 1, 0, 0, 0);//= get first day of today's year
+  customDateTable(today, firstDayOfYear);
+}
+
+//generates table of all transactions within two given dates
+function customDateTable(date1, date2){
+  //CHECK IF VALID DATE RANGE
+  if(date1 <= date2){
+    var inCorrectDate = " <caption>Please input correct date range</caption>";
     var table = document.getElementById("datatable");
-    table.innerHTML = rows;
+    table.innerHTML = inCorrectDate;
+  return;}
+  var rows = " <caption>Data from " + date1 + " through " + date2 + ".</caption>";
+  //WORK IN PROGRESS CODE GOES BELOW HERE
+
+
+
+
+
+
+
+  var table = document.getElementById("datatable");
+  table.innerHTML += rows;
+}
+
+//displays options for user to input a custom date range.
+function displayCustomDate(){
+  document.getElementById("customDate").classList.toggle("show");
+}
+
+
+//display Dropdown menus for custom date
+function showYear1(){document.getElementById("yearOne").classList.toggle("show"); insertYears(1);}
+function showMonth1(){document.getElementById("monthOne").classList.toggle("show");}
+function showDay1(){document.getElementById("dayOne").classList.toggle("show");}
+function showYear2(){document.getElementById("yearTwo").classList.toggle("show"); insertYears(2)}
+function showMonth2(){document.getElementById("monthTwo").classList.toggle("show");}
+function showDay2(){document.getElementById("dayTwo").classList.toggle("show");}
+
+//fills years into custom date dropdown
+function insertYears(oneOrTwo){
+  var yearList = "";
+
+  //Change to update to current year
+  for (var i = 22; i > 0; i--){
+    if(i>9){
+      //yearList += "<option value=20"+i+">20"+i+"</option>";
+      yearList += "<div onclick=\"changeResultYear(20"+i+", "+ oneOrTwo +")\">20"+i+"</div>";
+    }
+    else{
+      //yearList += "<option value=200"+i+">200"+i+"</option>";
+      yearList += "<div onclick=\"changeResultYear(200"+i+", "+ oneOrTwo +")\">200"+i+"</div>";
+    }
+  }
+
+  var whichYearDropdown;
+  if(oneOrTwo == 1){
+    whichYearDropdown = document.getElementById("yearOne");
+  }
+  if(oneOrTwo == 2){
+    whichYearDropdown = document.getElementById("yearTwo");
+  }
+  whichYearDropdown.innerHTML = yearList;
+
+}
+
+//changes the text displayed on the year button to the selected year
+function changeResultYear(year, oneOrTwo) {
+//var year = document.getElementById("yearOne").value;
+  if (oneOrTwo == 1){
+    document.getElementById("yearOneButtonText").innerHTML = year;
+  }
+  if (oneOrTwo == 2){
+    document.getElementById("yearTwoButtonText").innerHTML = year;
+  }
+
+}
+
+
+//changes the text displayed on the month button to the selected year
+function changeResultMonth(month, oneOrTwo) {
+switch(oneOrTwo){
+    case 1:
+      switch(month){
+        case 1:
+          document.getElementById("monthOneButtonText").innerHTML = "January";
+          break;
+        case 2:
+          document.getElementById("monthOneButtonText").innerHTML = "February";
+          break;
+        case 3:
+          document.getElementById("monthOneButtonText").innerHTML = "March";
+          break;
+        case 4:
+          document.getElementById("monthOneButtonText").innerHTML = "April";
+          break;
+        case 5:
+          document.getElementById("monthOneButtonText").innerHTML = "May";
+          break;
+        case 6:
+          document.getElementById("monthOneButtonText").innerHTML = "June";
+          break;
+        case 7:
+          document.getElementById("monthOneButtonText").innerHTML = "July";
+          break;
+        case 8:
+          document.getElementById("monthOneButtonText").innerHTML = "August";
+          break;
+        case 9:
+          document.getElementById("monthOneButtonText").innerHTML = "September";
+          break;
+        case 10:
+          document.getElementById("monthOneButtonText").innerHTML = "October";
+          break;
+        case 11:
+          document.getElementById("monthOneButtonText").innerHTML = "November";
+          break;
+        case 12:
+          document.getElementById("monthOneButtonText").innerHTML = "December";
+          break;
+      }
+      break;
+    case 2:
+      switch(month){
+        case 1:
+          document.getElementById("monthTwoButtonText").innerHTML = "January";
+          break;
+        case 2:
+          document.getElementById("monthTwoButtonText").innerHTML = "February";
+          break;
+        case 3:
+          document.getElementById("monthTwoButtonText").innerHTML = "March";
+          break;
+        case 4:
+          document.getElementById("monthTwoButtonText").innerHTML = "April";
+          break;
+        case 5:
+          document.getElementById("monthTwoButtonText").innerHTML = "May";
+          break;
+        case 6:
+          document.getElementById("monthTwoButtonText").innerHTML = "June";
+          break;
+        case 7:
+          document.getElementById("monthTwoButtonText").innerHTML = "July";
+          break;
+        case 8:
+          document.getElementById("monthTwoButtonText").innerHTML = "August";
+          break;
+        case 9:
+          document.getElementById("monthTwoButtonText").innerHTML = "September";
+          break;
+        case 10:
+          document.getElementById("monthTwoButtonText").innerHTML = "October";
+          break;
+        case 11:
+          document.getElementById("monthTwoButtonText").innerHTML = "November";
+          break;
+        case 12:
+          document.getElementById("monthTwoButtonText").innerHTML = "December";
+          break;
+    }
+    break;
+
+  }
+}
+
+//changes the text displayed on the day button to the selected day
+function changeResultDay(day, oneOrTwo) {
+  if(oneOrTwo ==1){
+    document.getElementById("dayOneButtonText").innerHTML = day;
+
+  }
+  else if(oneOrTwo ==2){
+    document.getElementById("dayTwoButtonText").innerHTML = day;
+  }
+}
+
+
+//Executes the custom report once the date fields have been filled
+function executeCustomDates(){
+  var monthNumberOne = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ].indexOf(document.getElementById("monthOneButtonText").innerHTML) + 1;
+
+  var firstDate = new Date(document.getElementById("yearOneButtonText").innerHTML, monthNumberOne, document.getElementById("dayOneButtonText").innerHTML, 0, 0, 0);
+  console.log(firstDate)
+
+
+  var monthNumberTwo = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ].indexOf(document.getElementById("monthTwoButtonText").innerHTML) + 1;
+
+  var secondDate = new Date(document.getElementById("yearTwoButtonText").innerHTML, monthNumberTwo, document.getElementById("dayTwoButtonText").innerHTML, 0, 0, 0);
+  console.log(secondDate)
+
+  customDateTable(firstDate, secondDate);
+
 }
