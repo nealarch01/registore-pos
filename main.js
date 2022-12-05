5ffa172ccf5523b6023346699e56e74e9359c255
myd28 <my.dang2028@gmail.com>
2022-12-04 19:19:06 -0800

    began building login page my 12/4

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, Menu } = require('electron');
+const { app,session, BrowserWindow, Menu } = require('electron');
 const path = require('path');
 
 const isMac = process.platform === 'darwin';
@@ -6,6 +6,33 @@ const isDev = process.env.NODE_ENV !== 'production';
 
 const { ipcMain } = require('electron');
 const RegistoreBackend = require('./node_modules/registore-business-logic/dist/index');
+let currentSession= null; 
+
+ipcMain.handle('getCookie', async (event, args) => {
+    let currentSession= session.defaultSession.cookies.get({ url: 'http://www.github.com' })
+    .then((cookies) => {
+      console.log(cookies)
+      return cookies; 
+    }).catch((error) => {
+      console.log(error)
+      return null;
+    })
+});
+ipcMain.handle('setCookie', async (event, args) => {
+    const { employeeId } = args; // Desctructure the args object
+    const cookie = { url: 'http://www.github.com', name: 'dummy_name', value: employeeId }
+session.defaultSession.cookies.set(cookie)
+  .then(() => {
+    // success
+  }, (error) => {
+    console.error(error)
+  })
+
+});
+ipcMain.handle('clearCookie', async (event, args) => {
+    session.defaultSession.clearStorageData([], (data) => {});
+
+});
 
 // Because we are returning values, we need to use ipcMain.handle, do not use ipcMain.on
 ipcMain.handle('getTransaction', async (event, args) => {
@@ -70,137 +97,137 @@ ipcMain.handle('deleteProductbySKU', async (event, args) => {
 ipcMain.handle('createNewEmployee', async (event, args) => {
     console.log(args);
     const { employee } = args; // Desctructure the args object
-    const response = await RegistoreBackend.ProductController.createNewEmployee(employee);
+    const response = await RegistoreBackend.EmployeeController.createNewEmployee(employee);
     return response;
 });
 ipcMain.handle('getEmployee', async (event, args) => {
     console.log(args);
     const { employeeId } = args; // Desctructure the args object
-    const response = await RegistoreBackend.ProductController.getEmployee(employeeId);
+    const response = await RegistoreBackend.EmployeeController.getEmployee(employeeId);
     return response;
 });
 ipcMain.handle('getAllEmployees', async (event) => {
-    const response = await RegistoreBackend.ProductController.getAllEmployees();
+    const response = await RegistoreBackend.EmployeeController.getAllEmployees();
     return response;
 });
 ipcMain.handle('updateEmployee', async (event, args) => {
     console.log(args);
     const { employeeId, first_name, last_name, phone_number, email, address, city, state, zipcode, password, hire_date, starting_amount } = args; // Desctructure the args object
-    const response = await RegistoreBackend.ProductController.updateEmployee(employeeId, first_name, last_name, phone_number, email, address, city, state, zipcode, password, hire_date, starting_amount);
+    const response = await RegistoreBackend.EmployeeController.updateEmployee(employeeId, first_name, last_name, phone_number, email, address, city, state, zipcode, password, hire_date, starting_amount);
     return response;
 });
 ipcMain.handle('deleteEmployee', async (event, args) => {
     console.log(args);
     const { employeeId } = args; // Desctructure the args object
-    const response = await RegistoreBackend.ProductController.deleteEmployee(employeeId);
+    const response = await RegistoreBackend.EmployeeController.deleteEmployee(employeeId);
     return response;
 });
 ipcMain.handle('createNewDiscount', async (event, args) => {
     console.log(args);
     const { discount } = args; // Desctructure the args object
-    const response = await RegistoreBackend.ProductController.createNewDiscount(discount);
+    const response = await RegistoreBackend.DiscountController.createNewDiscount(discount);
     return response;
 });
 ipcMain.handle('deleteDiscount', async (event, args) => {
     console.log(args);
     const { discountId } = args; // Desctructure the args object
-    const response = await RegistoreBackend.ProductController.deleteDiscount(discountId);
+    const response = await RegistoreBackend.DiscountController.deleteDiscount(discountId);
     return response;
 });
 ipcMain.handle('updateDiscount', async (event, args) => {
     console.log(args);
     const { discountId, newAmount } = args; // Desctructure the args object
-    const response = await RegistoreBackend.ProductController.updateDiscount(discountId, newAmount);
+    const response = await RegistoreBackend.DiscountController.updateDiscount(discountId, newAmount);
     return response;
 });
 ipcMain.handle('getAllDiscounts', async (event) => {
-    const response = await RegistoreBackend.ProductController.getAllDiscounts();
+    const response = await RegistoreBackend.DiscountController.getAllDiscounts();
     return response;
 });
 ipcMain.handle('getDiscount', async (event, args) => {
     console.log(args);
     const { id } = args; // Desctructure the args object
-    const response = await RegistoreBackend.ProductController.getDiscount(id);
+    const response = await RegistoreBackend.DiscountController.getDiscount(id);
     return response;
 });
 ipcMain.handle('createNewTransaction', async (event, args) => {
     console.log(args);
     const { transaction } = args; // Desctructure the args object
-    const response = await RegistoreBackend.ProductController.createNewTransaction(transaction);
+    const response = await RegistoreBackend.TransactionController.createNewTransaction(transaction);
     return response;
 });
 ipcMain.handle('addTransactionItems', async (event, args) => {
     console.log(args);
     const { transactionItems, transactionID } = args; // Desctructure the args object
-    const response = await RegistoreBackend.ProductController.addTransactionItems(transactionItems, transactionID);
+    const response = await RegistoreBackend.TransactionController.addTransactionItems(transactionItems, transactionID);
     return response;
 });
 ipcMain.handle('addTransactionDiscounts', async (event, args) => {
     console.log(args);
     const { discounts, transactionID } = args; // Desctructure the args object
-    const response = await RegistoreBackend.ProductController.addTransactionDiscounts(discounts, transactionID);
+    const response = await RegistoreBackend.TransactionController.addTransactionDiscounts(discounts, transactionID);
     return response;
 });
 ipcMain.handle('createCashTransaction', async (event, args) => {
     console.log(args);
     const { products, discounts, salespersonID } = args; // Desctructure the args object
-    const response = await RegistoreBackend.ProductController.createCashTransaction(products, discounts, salespersonID);
+    const response = await RegistoreBackend.TransactionController.createCashTransaction(products, discounts, salespersonID);
     return response;
 });
 ipcMain.handle('createCardTransaction', async (event, args) => {
     console.log(args);
     const { products, discounts, salespersonID } = args; // Desctructure the args object
-    const response = await RegistoreBackend.ProductController.createCardTransaction(products, discounts, salespersonID);
+    const response = await RegistoreBackend.TransactionController.createCardTransaction(products, discounts, salespersonID);
     return response;
 });
 ipcMain.handle('calculateTotal', async (event, args) => {
     console.log(args);
     const { products } = args; // Desctructure the args object
-    const response = await RegistoreBackend.ProductController.calculateTotal(products);
+    const response = await RegistoreBackend.TransactionController.calculateTotal(products);
     return response;
 });
 ipcMain.handle('calculateTotalWithDiscount', async (event, args) => {
     console.log(args);
     const { products, discount } = args; // Desctructure the args object
-    const response = await RegistoreBackend.ProductController.calculateTotalWithDiscount(products, discount);
+    const response = await RegistoreBackend.TransactionController.calculateTotalWithDiscount(products, discount);
     return response;
 });
 ipcMain.handle('getTransactionItems', async (event, args) => {
     console.log(args);
     const { transactionID } = args; // Desctructure the args object
-    const response = await RegistoreBackend.ProductController.getTransactionItems(transactionID);
+    const response = await RegistoreBackend.TransactionController.getTransactionItems(transactionID);
     return response;
 });
 ipcMain.handle('getTransactionDiscounts', async (event, args) => {
     console.log(args);
     const { transactionID } = args; // Desctructure the args object
-    const response = await RegistoreBackend.ProductController.getTransactionDiscounts(transactionID);
+    const response = await RegistoreBackend.TransactionController.getTransactionDiscounts(transactionID);
     return response;
 });
 ipcMain.handle('getAllTransactions', async (event) => {
-    const response = await RegistoreBackend.ProductController.getAllTransactions();
+    const response = await RegistoreBackend.TransactionController.getAllTransactions();
     return response;
 });
 ipcMain.handle('getTransactionsBySalesperson', async (event, args) => {
     console.log(args);
     const { salesPersonId } = args; // Desctructure the args object
-    const response = await RegistoreBackend.ProductController.getTransactionsBySalesperson(salesPersonId);
+    const response = await RegistoreBackend.TransactionController.getTransactionsBySalesperson(salesPersonId);
     return response;
 });
 ipcMain.handle('getTransactionsByTotal', async (event) => {
-    const response = await RegistoreBackend.ProductController.getTransactionsByTotal();
+    const response = await RegistoreBackend.TransactionController.getTransactionsByTotal();
     return response;
 });
 ipcMain.handle('updateTransactionSalesperson', async (event, args) => {
     console.log(args);
     const { transactionId, salesPersonId } = args; // Desctructure the args object
-    const response = await RegistoreBackend.ProductController.updateTransactionSalesperson(transactionId, salesPersonId);
+    const response = await RegistoreBackend.TransactionController.updateTransactionSalesperson(transactionId, salesPersonId);
     return response;
 });
 ipcMain.handle('deleteTransaction', async (event, args) => {
     console.log(args);
     const { transactionId } = args; // Desctructure the args object
-    const response = await RegistoreBackend.ProductController.deleteTransaction(transactionId);
+    const response = await RegistoreBackend.TransactionController.deleteTransaction(transactionId);
     return response;
 });
 // create the main window
@@ -232,6 +259,7 @@ function createItemInfo() {
     itemInfo.loadFile(path.join(__dirname, './render/itemInfo.html'));
 }
 
+
 // app is ready
 app.whenReady().then(() => {
     createMainWindow();
@@ -243,6 +271,12 @@ app.whenReady().then(() => {
         if (BrowserWindow.getAllWindows().length === 0) {
             createMainWindow();
         }
+        session.defaultSession.cookies.get({})
+  .then((cookies) => {
+    console.log(cookies)
+  }).catch((error) => {
+    console.log(error)
+  })
     });
 });
 
