57819bdcc5e67bf311f23286b7ef999725209f44
dannyho0201 <56488327+dannyho0201@users.noreply.github.com>
2022-12-07 17:03:53 -0800

    implemented add product feature

--- render/js/inventory.js
+++ render/js/inventory.js
@@ -1,9 +1,25 @@
 var inventoryTable = document.getElementById('inventory-list');
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 var products = null;
-let search = document.getElementById('search');
+var removeCheckbox = document.getElementsByClassName('remove-checkbox');
+var search = document.getElementById('search');
+var productName = document.getElementById('product-name');
+var productSku = document.getElementById('product-sku');
+var productPrice = document.getElementById('product-price');
+var productQuantity = document.getElementById('product-quantity');
+var productSummary = document.getElementById('product-summary');
+var productBrand = document.getElementById('product-brand');
+var productCategory = document.getElementById('product-category');
+var productSupplier = document.getElementById('product-supplier');
+var modalSubmit = document.getElementById('modal-submit');
+// Get the modal
+var modal = document.getElementById('myModal');
+// Get the button that opens the modal
+var btn = document.getElementById('addInventory');
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName('close')[0];
 
 search.addEventListener('keydown', (e) => {
-    console.log(search.value);
     let mySearch = search.value;
     if (e.key == 'Enter' && mySearch != '') {
         searchProduct(mySearch);
@@ -13,8 +29,26 @@ search.addEventListener('keydown', (e) => {
 });
 document.addEventListener('DOMContentLoaded', getAllProductsHTML);
 
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+    modal.style.display = 'block';
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = 'none';
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+};
+
 function resetTable() {
     inventoryTable.innerHTML = `<tr>
+    <th></th>
     <th>SKU</th>
     <th>Name</th>
     <th>Price</th>
@@ -23,8 +57,58 @@ function resetTable() {
   </tr>`;
 }
 
-function addProduct() {}
-function removeProduct() {}
+// check if our string input is empty.
+function isEmpty(str) {
+    return !str || str.length === 0;
+}
+//Check if our string input contains a number.
+function hasNumber(myString) {
+    return /\d/.test(myString);
+}
+
+function highlightRow() {}
+
+async function addProduct() {
+    // When you call this it will add it to the DB for you.
+    if (isEmpty(productSku.value)) {
+        alert('Please enter a product sku!');
+    } else if (isEmpty(productName.value)) {
+        alert('Please enter a product title!');
+    } else if (isEmpty(productBrand.value)) {
+        alert('Please enter a product brand!');
+    } else if (isEmpty(productSummary.value)) {
+        alert('Please enter a product summary!');
+    } else if (isEmpty(productPrice.value)) {
+        alert('Please enter a product price!');
+    } else if (isEmpty(productQuantity.value)) {
+        alert('Please enter a product quantity!');
+    } else if (isEmpty(productCategory.value)) {
+        alert('Please enter a product category!');
+    } else if (isEmpty(productSupplier.value)) {
+        alert('Please enter a product supplier!');
+    }
+    const newProduct = await Backend.ProductBuilder(
+        productSku.value,
+        productName.value,
+        productBrand.value,
+        productSummary.value,
+        productPrice.value,
+        productQuantity.value,
+        productCategory.value,
+        0,
+        productSupplier.value
+    );
+    if (newProduct.error !== null) {
+        console.log('ERROR' + newProduct.error);
+        return;
+    } else {
+        //This is the code that will execute if we have a valid product.
+        //need to figure out how to refresh page
+    }
+}
+
+async function removeProduct() {}
+
 function searchProduct(name) {
     if (products == null) {
         return;
@@ -33,7 +117,8 @@ function searchProduct(name) {
         //Now that we have the defaults set, we can display the results
         products.forEach((product) => {
             if (product.title.startsWith(name))
-                inventoryTable.innerHTML += `<tr id="${product.sku}"><td>${product.sku}</td><td>${product.title}</td><td>${product.price}</td><td>${product.quantity}</td><td>${product.summary}</td></tr>`;
+                var checkboxID = product.sku + '-checkbox';
+            inventoryTable.innerHTML += `<tr id="${product.sku}"><td><input type="checkbox" id="${checkboxID}" class="remove-check"/></td><td>${product.sku}</td><td>${product.title}</td><td>${product.price}</td><td>${product.quantity}</td><td>${product.summary}</td></tr>`;
         });
     }
 }
@@ -52,7 +137,8 @@ async function getAllProductsHTML() {
     products = response.data;
     resetTable();
     products.forEach((product) => {
-        inventoryTable.innerHTML += `<tr id="${product.sku}"><td>${product.sku}</td><td>${product.title}</td><td>${product.price}</td><td>${product.quantity}</td><td>${product.summary}</td></tr>`;
+        var checkboxID = product.sku + '-checkbox';
+        inventoryTable.innerHTML += `<tr id="${product.sku}"><td><input type="checkbox" id="${checkboxID}" class="remove-check"/></td><td>${product.sku}</td><td>${product.title}</td><td>${product.price}</td><td>${product.quantity}</td><td>${product.summary}</td></tr>`;
     });
 }
 function sortProducts() {}
